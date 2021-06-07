7ce95463c79fb8748311f4444cadfe1181c7d195
Luigi <69606645+mostlikelyluigi@users.noreply.github.com>
2021-06-07 17:35:20 +0100

    This is a new commit for what I originally planned to be amended

--- main.cpp
+++ main.cpp
@@ -2,19 +2,53 @@
 #include "ball.h"
 #include <sstream>
 #include <cstdlib>
+#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 
 /*Side note: header files list the publicibly accessible instance variables and methods in the class
              definition; .cpp files are used to implement those methods.*/
 
 using namespace sf;
+using namespace std;
+
+//Define the sfx path
+#define HIT_SOUND_FILEPATH "Music/PongSound.wav"
 
 int main()
 {
     //Create a window of 1024x768 pixels in size
     int windowWidth = 1024;
     int windowHeight = 768;
+
+
+    //Loading the soundbuffer for the sfx
+    sf::SoundBuffer _hitSoundBuffer;
+    sf::Sound _hitSound;
+
+    //Load the sfx file
+    if (!_hitSoundBuffer.loadFromFile( HIT_SOUND_FILEPATH ))
+    {
+        std::cout << "Error loading the sfx file" << std::endl;
+    }
+
+    _hitSound.setBuffer(_hitSoundBuffer);
+
+
+    //PLAY BACKGROUND MUSIC
+    sf::Music backgroundTheme;
+    //test
+   //open the music file
+    if (!backgroundTheme.openFromFile("Music/backgroundTheme.ogg"))
+    {
+        std::cout << "Error importing the background theme file" << std::endl;
+    }
+    //Make sure the music loops
+    backgroundTheme.setLoop(true);
+    //Play the background theme
+    backgroundTheme.play();
+
 
     //Make the title of the window "PONG"
     RenderWindow window(VideoMode(windowWidth, windowHeight), "PONG");
@@ -114,6 +148,8 @@ int main()
         {
             //Hit detected so reverse the ball and score a point
             ball.reboundBatOrTop();
+
+            _hitSound.play(); //Play the sfx sound when the ball hits the bat
         }
 
         ball.update();
